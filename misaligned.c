30aa8753a1843216210babe8d47ed979c5cf380c
AbhijeetArjun-Shinde <91106940+AbhijeetArjun-Shinde@users.noreply.github.com>
2022-01-24 11:34:34 +0530

    Update misaligned.c

--- misaligned.c
+++ misaligned.c
@@ -4,24 +4,29 @@
 const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
 const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
 
+int numberOfMajorColors =sizeof(MajorColor) / sizeof(MajorColor[0]);
+int numberOfMinorColors =sizeof(MinorColor) / sizeof(MinorColor[0]);
+
+void PrintOnConsole( int minorcolorid, int majorcolorid){
+printf("%d | %s | %s\n", minorcolorid * 5 + majorcolorid, majorColor[majorcolorid], minorColor[minorcolorid]);
+}
+
 
 int printColorMap() {
     int i = 0, j = 0;
-    for(i = 0; i < 5; i++) {
-        for(j = 0; j < 5; j++) {
+    for(i = 0; i < numberOfMinorColors; i++) {
+        for(j = 0; j < numberOfMajorColors; j++) {
             PrintOnConsole(i,j);    
         }
     }
     return i * j;
 }
 
-PrintOnConsole( int majorcolorid, int minorcolorid){
-printf("%d | %s | %s\n", majorcolorid * 5 + minorcolorid, majorColor[majorcolorid], minorColor[minorcolorid]);
-}
+
     
 int main() {
     int result = printColorMap();
-    assert(result == 25);
+    assert(result == 24);
     printf("All is well (maybe!)\n");
     return 0;
 }
