ba3539049c9ffa282b14836784d7c9d2bf4646a7
AbhijeetArjun-Shinde <91106940+AbhijeetArjun-Shinde@users.noreply.github.com>
2022-01-24 11:52:42 +0530

    Update alerter.c

--- alerter.c
+++ alerter.c
@@ -28,6 +28,7 @@ void alertInCelcius(float farenheit) {
         // However, this code doesn't count failures!
         // Add a test below to catch this bug. Alter the stub above, if needed.
         alertFailureCount += 0;
+        assert(alertFailureCount == (alertFailureCount+1));
     }
 }
 
