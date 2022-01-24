f0184465c99a77e8d7ca6ed1be29b91eeb2d4648
AbhijeetArjun-Shinde <91106940+AbhijeetArjun-Shinde@users.noreply.github.com>
2022-01-24 11:49:36 +0530

    Update alerter.c

--- alerter.c
+++ alerter.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
 #include <assert.h>
 
+#define MAX_TEMP_THRESHOLD_CEL 180
+
 int alertFailureCount = 0;
 
 int networkAlertStub(float celcius) {
     printf("ALERT: Temperature is %.1f celcius.\n", celcius);
+    if(celcius > MAX_TEMP_THRESHOLD_CEL){
+        return 500;
+    }
+    else{
+        return 200;
+    }
     // Return 200 for ok
     // Return 500 for not-ok
     // stub always succeeds and returns 200
