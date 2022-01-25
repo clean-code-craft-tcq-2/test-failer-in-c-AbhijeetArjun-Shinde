76ead6b0d3a321bfd01b9946ba301467e99feeb2
AbhijeetArjun-Shinde <91106940+AbhijeetArjun-Shinde@users.noreply.github.com>
2022-01-25 11:36:01 +0530

    Update misaligned.c

--- misaligned.c
+++ misaligned.c
@@ -4,25 +4,28 @@
 int StringMakerFuncCalls =0 ;
 int PrintConsoleFuncCalls =0 ;
 
-void PrintStringMaker( int MajorColorId , int MinorColorId);
+int PrintStringMaker( int MajorColorId , int MinorColorId);
 void PrintOnConsole(int PairNum, const char *MajorColor, const char *MinorColor);
 
 int printColorMap() {
     int i = 0, j = 0;
     for(i = 0; i < 5; i++) {
         for(j = 0; j < 5; j++) {
-            PrintStringMaker(i,j);     
+         int PairNum = PrintStringMaker(i,j); 
+         assert ( (PairNum >=0 && PairNum <=25));
         }
     }
     return i * j;
 }
 
-void PrintStringMaker( int MajorColorId , int MinorColorId){
+int PrintStringMaker( int MajorColorId , int MinorColorId){
     const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
     const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
     PrintOnConsole( (MajorColorId*5+ MinorColorId) , majorColor[MajorColorId] , minorColor[MajorColorId]);
     StringMakerFuncCalls+=1;
+    return (MajorColorId*5+ MinorColorId);
     }
+
 void PrintOnConsole(int PairNum,const char *MajorColor ,const char *MinorColor){
     printf("%d | %s | %s\n",PairNum, MajorColor, MinorColor);
     PrintConsoleFuncCalls +=1;
@@ -31,6 +34,7 @@ void PrintOnConsole(int PairNum,const char *MajorColor ,const char *MinorColor){
 int main() {
     int result = printColorMap();
     assert(result == 25);
+    assert
     assert(StringMakerFuncCalls == 25);
     assert(PrintConsoleFuncCalls == 25);
     printf("All is well (maybe!)\n");
