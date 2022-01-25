2d37674a72b24e379f299a9e5099e5220c3dc4d0
AbhijeetArjun-Shinde <91106940+AbhijeetArjun-Shinde@users.noreply.github.com>
2022-01-25 09:28:26 +0530

    Update misaligned.c

--- misaligned.c
+++ misaligned.c
@@ -1,16 +1,26 @@
 #include <stdio.h>
 #include <assert.h>
 
+void PrintStringMaker( int MajorColorId , int MinorColorId);
+void PrintOnConsole(int PairNum, int MajorColorId , int MinorColorId);
 int printColorMap() {
-    const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
-    const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+    
     int i = 0, j = 0;
     for(i = 0; i < 5; i++) {
         for(j = 0; j < 5; j++) {
-            printf("%d | %s | %s\n", i * 5 + j, majorColor[i], minorColor[i]);
+            PrintStringMaker(i,j);     
         }
     }
     return i * j;
+}
+
+int PrintStringMaker( int MajorColorId , int MinorColorId){
+    const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
+    const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+    PrintOnConsole( (MajorColorId*5+ MinorColorId) , majorColor[MajorColorId] , minorColor[MajorColorId]);
+    }
+void PrintOnConsole(int PairNum, char *MajorColor , char *MinorColor){
+    printf("%d | %s | %s\n",PairNum, MajorColor, MinorColor);
 }
 
 int main() {
