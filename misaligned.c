d4a3865d7af9eedc13b2e3a764b28be1a769fa72
AbhijeetArjun-Shinde <91106940+AbhijeetArjun-Shinde@users.noreply.github.com>
2022-01-25 09:50:13 +0530

    Update misaligned.c

--- misaligned.c
+++ misaligned.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <assert.h>
 
+int StringMakerFuncCalls =0 ;
+int PrintConsoleFuncCalls =0 ;
+
 void PrintStringMaker( int MajorColorId , int MinorColorId);
 void PrintOnConsole(int PairNum, int MajorColorId , int MinorColorId);
+
 int printColorMap() {
-    
     int i = 0, j = 0;
     for(i = 0; i < 5; i++) {
         for(j = 0; j < 5; j++) {
@@ -18,14 +21,18 @@ void PrintStringMaker( int MajorColorId , int MinorColorId){
     const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
     const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
     PrintOnConsole( (MajorColorId*5+ MinorColorId) , majorColor[MajorColorId] , minorColor[MajorColorId]);
+    StringMakerFuncCalls+=1;
     }
 void PrintOnConsole(int PairNum, char *MajorColor , char *MinorColor){
     printf("%d | %s | %s\n",PairNum, MajorColor, MinorColor);
+    PrintConsoleFuncCalls +=1;
 }
 
 int main() {
     int result = printColorMap();
     assert(result == 25);
+    assert(StringMakerFuncCalls == 25);
+    assert(PrintConsoleFuncCalls == 25);
     printf("All is well (maybe!)\n");
     return 0;
 }
