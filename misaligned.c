e7a6240987e2f82d85185f7d1cf14965451a042d
AbhijeetArjun-Shinde <91106940+AbhijeetArjun-Shinde@users.noreply.github.com>
2022-02-01 17:33:03 +0530

    Update misaligned.c

--- misaligned.c
+++ misaligned.c
@@ -40,7 +40,7 @@ int printColorMap() {
     int i = 0, j = 0;
     for(i = 0; i < 5; i++) {
         for(j = 0; j < 5; j++) {
-         PrintStringCreater(i,j);  
+         PrintStringCreator(i,j);  
         }
     }
     return i * j;
