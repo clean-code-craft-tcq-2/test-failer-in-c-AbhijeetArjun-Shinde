a0a0a64fe91b2a2ec77111ba01832207585e8f12
AbhijeetArjun-Shinde <91106940+AbhijeetArjun-Shinde@users.noreply.github.com>
2022-02-01 17:07:46 +0530

    Update misaligned.c

--- misaligned.c
+++ misaligned.c
@@ -4,40 +4,57 @@
 const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
 const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
 
-int StringMakerFuncCalls =0 ;
-int PrintConsoleFuncCalls =0 ;
-int mismatch =0;
-const char *ActualColorGuide[25][2];
+
+typedef struct {
+     int PairID;
+     char MajorColor[8];
+     char MinorColor[8];
+}ColorPair; 
+
+ColorPair *ActualColorMap[25];
+
+int PairIdMismatchCnt = 0;
+int MajorColorMismatchCnt = 0;
+int MinorColorMismatchCnt = 0;
 
 void ActualColorMap() {
     int i = 0, j = 0;
     for(i = 0; i < 5; i++) {
         for(j = 0; j < 5; j++) {
-         ActualColorGuide[i+j][0]=majorColor[i];  
-         ActualColorGuide[i+j][1]=minorColor[j];
+             ActualColorMap.PairID[i*5+j]= i*5+j ;
+             strcpy(ActualColorMap.MajorColor[i*5+j],majorColor[i] );
+             strcpy(ActualColorMap.MinorColor[i*5+j],minorColor[j] );
         }
     }
 }
 
-void PrintStringMaker( int MajorColorId , int MinorColorId);
+void PrintStringCreator( int MajorColorId , int MinorColorId);
 void PrintOnConsole(int PairNum, const char *MajorColor, const char *MinorColor);
 
 int printColorMap() {
     int i = 0, j = 0;
     for(i = 0; i < 5; i++) {
         for(j = 0; j < 5; j++) {
-         PrintStringMaker(i,j);  
+         PrintStringCreater(i,j);  
         }
     }
     return i * j;
 }
 
-void PrintStringMaker( int MajorColorId , int MinorColorId){
+void PrintStringCreater( int MajorColorId , int MinorColorId){
     PrintOnConsole( (MajorColorId*5+ MinorColorId) , majorColor[MajorColorId] , minorColor[MajorColorId]);
-    StringMakerFuncCalls+=1;
-        if(((MajorColorId*5+ MinorColorId) <=0 || (MajorColorId*5+ MinorColorId) >25) || (majorColor[MajorColorId] != ActualColorGuide[MajorColorId*5+ MinorColorId][0]) || (minorColor[MajorColorId]!=ActualColorGuide[MajorColorId*5+ MinorColorId][1])){
-             mismatch = 1;
-         }
+    // Checking for PairNumber Mismatch
+    if(ActualColorMap.PairID[MajorColorId*5+MajorColorId] != MajorColorId*5+MajorColorId) {
+        PairIdMismatchCnt +=1;
+    }
+    //Checking for Major Color mismatch
+    else if (ActualColorMap.MajorColor[MajorColorId*5+MajorColorId] != majorColor[MajorColorId]) {
+        MajorColorMismatchCnt +=1 ;
+    }
+    // Checking for minor color mismatch
+    else if(ActualColorMap.MinorColor[MajorColorId*5+MajorColorId] != minorColor[MajorColorId]){
+        MinorColorMismatchCnt +=1 ;
+    }
     }
 
 void PrintOnConsole(int PairNum,const char *MajorColor ,const char *MinorColor){
@@ -48,9 +65,9 @@ void PrintOnConsole(int PairNum,const char *MajorColor ,const char *MinorColor){
 int main() {
     int result = printColorMap();
     assert(result == 25);
-    assert ( mismatch ==0);
-    assert(StringMakerFuncCalls == 25);
-    assert(PrintConsoleFuncCalls == 25);
+    assert ( PairIdMismatchCnt ==0);
+    assert(MajorColorMismatchCnt == 0);
+    assert(MinorColorMismatchCnt == 0);
     printf("All is well (maybe!)\n");
     return 0;
 }
